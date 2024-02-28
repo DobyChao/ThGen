133efcec5cd87a26b307655d9b532c5578b7f6e0
DobyChao <106363380+DobyChao@users.noreply.github.com>
2024-02-28 22:11:48 +0800

    fix bugs & add extensions

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from pywebio import *
 from pywebio.output import *
 from pywebio.pin import *
 
+TYPE_MAP = {
+    "str": "text",
+    "int": "number",
+}
+
 
 class JsonEditorGUI:
     add_icon = "\U00002795"
@@ -14,6 +19,7 @@ class JsonEditorGUI:
     refresh_icon = "\U0001F504"
 
     def __init__(self, config="template.json"):
+        self.ext_args = None
         self.config = config
         self.configs = [f for f in os.listdir('config') if f.endswith('.json')]
         with open(f"config/{config}", 'r') as f:
@@ -25,6 +31,7 @@ class JsonEditorGUI:
         self.config_select()
         self.essential()
         self.categories()
+        self.extensions()
 
     @use_scope("ROOT", clear=True)
     def home(self):
@@ -34,13 +41,14 @@ class JsonEditorGUI:
             put_tabs([
                 {"title": "基本设置", "content": put_scope("essential")},
                 {"title": "类别设置", "content": put_scope("categories")},
+                {"title": "扩展", "content": put_scope("extensions")},
             ])
         ], size="25% 5% 70%")
 
     @use_scope("config_select", clear=True)
     def config_select(self):
         self.configs = [f for f in os.listdir('config') if f.endswith('.json')]
-        options = [(f.split(".")[0], f) for f in self.configs if f.endswith('.json')]
+        options = [(f.split(".")[0], f) for f in self.configs]
         put_select("config", label="Config", options=options, value=self.config)
 
         put_buttons([self.save_icon, self.refresh_icon, self.add_icon, self.delete_icon],
@@ -50,30 +58,32 @@ class JsonEditorGUI:
 
     @use_scope("essential", clear=True)
     def essential(self):
+        unique_prefix = "_ess-"
         data_ess = self.data['essential']
-        put_input("name", label="数据集名", value=data_ess['name'])
-        put_input("dataset_path", label="数据集路径", value=data_ess['dataset_path'])
-        put_select("dataset_type", label="数据集类型", options=[("训练集", "train"),
-                                                                ("验证集", "val"),
-                                                                ("测试集", "test")], value=data_ess['dataset_type'])
-        put_select("label_type", label="标签形式", options=[('掩码', 'mask'), ('YOLO标注框', 'yolo_detect')],
-                   value=data_ess['label_type'])
+        put_input(unique_prefix + "name", label="数据集名", value=data_ess['name'])
+        put_input(unique_prefix + "dataset_path", label="数据集路径", value=data_ess['dataset_path'])
+        put_select(unique_prefix + "dataset_type", label="数据集类型", options=[("训练集", "train"),
+                                                                                ("验证集", "val"),
+                                                                                ("测试集", "test")],
+                   value=data_ess['dataset_type'])
+        put_select(unique_prefix + "label_type", label="标签形式",
+                   options=[('掩码', 'mask'), ('YOLO标注框', 'yolo_detect')], value=data_ess['label_type'])
 
         bg_enabled = "background" in self.data['categories']
 
-        put_select("use_empty_background", label="背景样式",
+        put_select(unique_prefix + "use_empty_background", label="背景样式",
                    options=[dict(label='纯黑背景', value=True),
                             dict(label='自定义背景' + ("" if bg_enabled else "（需要添加名为\"background\"的类别）"),
                                  value=False, disabled=not bg_enabled)],
                    value=data_ess['use_empty_background'])
-        put_input("init_seed", label="初始化种子", type='number', value=data_ess['init_seed'])
+        put_input(unique_prefix + "init_seed", label="初始化种子", type='number', value=data_ess['init_seed'])
 
-        pin_on_change("name", onchange=self.set_name, clear=True)
-        pin_on_change("dataset_path", onchange=self.set_dataset_path, clear=True)
-        pin_on_change("dataset_type", onchange=self.set_dataset_type, clear=True)
-        pin_on_change("label_type", onchange=self.set_label_type, clear=True)
-        pin_on_change("use_empty_background", onchange=self.set_use_empty_background, clear=True)
-        pin_on_change("init_seed", onchange=self.set_init_seed, clear=True)
+        pin_on_change(unique_prefix + "name", onchange=self.set_name, clear=True)
+        pin_on_change(unique_prefix + "dataset_path", onchange=self.set_dataset_path, clear=True)
+        pin_on_change(unique_prefix + "dataset_type", onchange=self.set_dataset_type, clear=True)
+        pin_on_change(unique_prefix + "label_type", onchange=self.set_label_type, clear=True)
+        pin_on_change(unique_prefix + "use_empty_background", onchange=self.set_use_empty_background, clear=True)
+        pin_on_change(unique_prefix + "init_seed", onchange=self.set_init_seed, clear=True)
 
     @use_scope("categories", clear=True)
     def categories(self):
@@ -81,8 +91,61 @@ class JsonEditorGUI:
             self.category(category)
         put_button("添加类别", onclick=self.add_category)
 
+    @use_scope("extensions", clear=True)
+    def extensions(self):
+        with open("extensions/arguments.json", 'r') as f:
+            self.ext_args = json.load(f)
+        for ext_name in self.ext_args:
+            self.extension(ext_name)
+
+    def extension(self, ext_name):
+        unique_prefix = f"_ext_{ext_name}-"
+        with use_scope(unique_prefix + "scope", clear=True):
+            ext_arg = self.ext_args[ext_name]
+            content = []
+            if "description" in ext_arg:
+                content.append(put_text(f"描述：{ext_arg['description']}"))
+            if "entry" in ext_arg:
+                content.append(put_text(f"程序入口：{ext_arg['entry']}"))
+            else:
+                raise ValueError(f"扩展{ext_name}缺少程序入口")
+
+            def up_exec():
+                nonlocal ext_arg
+                ext_arg['exec'] = "python " + ext_arg['entry']
+                for arg in ext_arg.get("args", []):
+                    value = pin[unique_prefix + arg['name']]
+                    if value:
+                        ext_arg['exec'] += f" --{arg['name']} {pin[unique_prefix + arg['name']]}"
+                # print(f"{ext_name}_exec", ext_arg['exec'])
+                pin_update(unique_prefix + "_exec", value=ext_arg['exec'])
+
+            up_exec()
+
+            for arg in ext_arg.get("args", []):
+                content.append(
+                    put_input(unique_prefix + arg['name'], label=arg['name'],
+                              type=TYPE_MAP.get(arg['type'], arg['type']), placeholder=arg['help'])
+                )
+                pin_on_change(unique_prefix + arg['name'], onchange=lambda x: up_exec(), clear=True)
+
+            content.append(
+                put_input(unique_prefix + "_exec", label="命令行预览", value=ext_arg['exec'], readonly=True)
+            )
+
+            def exec_command():
+                # print(ext_arg['exec'])
+                os.system(ext_arg['exec'])
+
+            content.append(
+                put_button("执行", onclick=exec_command)
+            )
+
+            put_collapse(ext_name, content)
+
     def category(self, category):
-        with use_scope(f"categories_{category}", clear=True):
+        unique_prefix = f"_cat_{category}-"
+        with use_scope(unique_prefix + "scope", clear=True):
             data_cat = self.data['categories'][category]
             options_all = [f for f in os.listdir(f"{data_cat['path']}")] if data_cat['path'] else []
 
@@ -91,48 +154,47 @@ class JsonEditorGUI:
                 nonlocal options_all
                 if set(options_all) != set(new_options_all):
                     options_all = new_options_all
-                    pin_update(f"{category}_checked", options=options_all)
+                    pin_update(unique_prefix + "checked", options=options_all)
                 return new_options_all
 
-            set_checked = partial(self.set_category_checked, category, func=set_ops)
+            set_checked = partial(self.set_category_checked, category, unique_prefix=unique_prefix, func=set_ops)
 
             put_row([
                 put_collapse(category, [
-                    put_checkbox(f"{category}_enabled", options=[("启用", "enabled", data_cat['enabled'])]),
-                    put_input(f"{category}_path", label="路径", value=data_cat['path']),
-                    put_input(f"{category}_cls", label="类别id", type='number', value=data_cat['cls']),
+                    put_checkbox(unique_prefix + "enabled", options=[("启用", "enabled", data_cat['enabled'])]),
+                    put_input(unique_prefix + "path", label="路径", value=data_cat['path']),
+                    put_input(unique_prefix + "cls", label="类别id", type='number', value=data_cat['cls']),
                     put_row([
-                        put_input(f"{category}_min_num", label="最小数量", type='number', value=data_cat['min_num']),
+                        put_input(unique_prefix + "min_num", label="最小数量", type='number', value=data_cat['min_num']),
                         None,
-                        put_input(f"{category}_max_num", label="最大数量", type='number', value=data_cat['max_num']),
+                        put_input(unique_prefix + "max_num", label="最大数量", type='number', value=data_cat['max_num']),
                         None
                     ], size="1fr 0.2fr 1fr 5fr"),
-                    put_checkbox(f"{category}_rotate", options=[("随机旋转", "rotate", data_cat['rotate'])]),
+                    put_checkbox(unique_prefix + "rotate", options=[("随机旋转", "rotate", data_cat['rotate'])]),
+                    put_markdown("""---
+                                    子类别筛选"""),
+                    # put_text("子类别"),
                     put_row([
-                        put_collapse("子类别", content=[
-                            put_scrollable(
-                                put_checkbox(f"{category}_checked", options=options_all, value=data_cat['checked']),
-                                height=(0, 200)),
-                            put_buttons(["全选", "全不选"],
-                                        onclick=[lambda: set_checked(options_all), lambda: set_checked([])])
-                        ], open=True),
+                        put_select(unique_prefix + "checked", options=options_all,
+                                   value=data_cat['checked'], multiple=True),
                         None,
                         put_button(self.refresh_icon, onclick=lambda: set_checked(data_cat['checked']))
-                    ], size="88% 2% 10%")
+                    ], size="88% 2% 10%"),
+                    put_buttons(["全选", "全不选"], onclick=[lambda: set_checked(options_all), lambda: set_checked([])])
                 ]),
                 None,
                 put_button(self.delete_icon, onclick=lambda: self.delete_category(category))
             ], size="88% 2% 10%")
 
-            pin_on_change(f"{category}_enabled", onchange=lambda x: self.set_category_enabled(category, x), clear=True)
-            pin_on_change(f"{category}_path", onchange=lambda x: self.set_category_path(category, x), clear=True)
-            pin_on_change(f"{category}_cls", onchange=lambda x: self.set_category_cls(category, x), clear=True)
-            pin_on_change(f"{category}_min_num", onchange=lambda x: self.set_category_min_max(category, min_num=x),
+            pin_on_change(unique_prefix + "enabled", onchange=lambda x: self.set_category_enabled(category, x), clear=True)
+            pin_on_change(unique_prefix + "path", onchange=lambda x: self.set_category_path(category, x), clear=True)
+            pin_on_change(unique_prefix + "cls", onchange=lambda x: self.set_category_cls(category, x), clear=True)
+            pin_on_change(unique_prefix + "min_num", onchange=lambda x: self.set_category_min_max(category, min_num=x),
                           clear=True)
-            pin_on_change(f"{category}_max_num", onchange=lambda x: self.set_category_min_max(category, max_num=x),
+            pin_on_change(unique_prefix + "max_num", onchange=lambda x: self.set_category_min_max(category, max_num=x),
                           clear=True)
-            pin_on_change(f"{category}_rotate", onchange=lambda x: self.set_category_rotate(category, x), clear=True)
-            pin_on_change(f"{category}_checked", onchange=set_checked, clear=True)
+            pin_on_change(unique_prefix + "rotate", onchange=lambda x: self.set_category_rotate(category, x), clear=True)
+            pin_on_change(unique_prefix + "checked", onchange=set_checked, clear=True)
 
     def set_config(self, new_config):
         self.config = new_config
@@ -222,12 +284,12 @@ class JsonEditorGUI:
     def set_category_rotate(self, category, rotate):
         self.data['categories'][category]['rotate'] = len(rotate) > 0
 
-    def set_category_checked(self, category, checked, func):
-        all = func()
-        if not set(checked).issubset(set(all)):
+    def set_category_checked(self, category, checked, unique_prefix, func):
+        options_all = func()
+        if not set(checked).issubset(set(options_all)):
             checked = []
         self.data['categories'][category]['checked'] = checked
-        pin_update(f"{category}_checked", value=checked)
+        pin_update(unique_prefix + "checked", value=checked)
 
     def add_category(self):
         def add_confirm():
@@ -264,4 +326,4 @@ def main():
 
 
 if __name__ == '__main__':
-    start_server(main, debug=True, port=8080, auto_open_webbrowser=True, cdn=False)
+    start_server(main, debug=True, port=8081, auto_open_webbrowser=True, cdn=False)
---
+++ extensions/slice.py
@@ -0,0 +1,63 @@
+import os
+import argparse
+from PIL import Image
+
+
+def slice_image(image_path, output_dir, top=0, left=0, bottom=-1, right=-1,
+                slice_height=None, slice_rows=None, slice_width=None, slice_cols=None):
+    image_name = os.path.basename(image_path)
+    print(image_name)
+    image = Image.open(image_path)
+
+    if bottom == -1:
+        bottom = image.height
+    if right == -1:
+        right = image.width
+
+    if slice_height and slice_rows:
+        raise ValueError("Please specify either slice height or slice rows, not both.")
+    if slice_width and slice_cols:
+        raise ValueError("Please specify either slice width or slice columns, not both.")
+
+    if slice_height:
+        slice_rows = (bottom - top) // slice_height
+    else:
+        slice_height = (bottom - top) // slice_rows
+
+    if slice_width:
+        slice_cols = (right - left) // slice_width
+    else:
+        slice_width = (right - left) // slice_cols
+
+    if (bottom - top) % slice_height != 0 or (right - left) % slice_width != 0:
+        raise ValueError("The specified slice size does not evenly divide the image size.")
+
+    if not os.path.exists(output_dir):
+        print(f"Creating directory {output_dir}")
+        os.makedirs(output_dir)
+
+    for i in range(slice_rows):
+        for j in range(slice_cols):
+            slice = image.crop((left + j * slice_width,
+                                top + i * slice_height,
+                                left + (j + 1) * slice_width,
+                                top + (i + 1) * slice_height))
+            slice.save(f"{output_dir}/{image_name}_{i}_{j}.png")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Slice an image into smaller pieces.')
+    parser.add_argument('--image_path', type=str, help='Path to the image file.')
+    parser.add_argument('--output_dir', type=str, help='Path to the output directory.')
+    parser.add_argument('--top', type=int, default=0, help='Top coordinate of the slice area.')
+    parser.add_argument('--left', type=int, default=0, help='Left coordinate of the slice area.')
+    parser.add_argument('--bottom', type=int, default=-1, help='Bottom coordinate of the slice area.')
+    parser.add_argument('--right', type=int, default=-1, help='Right coordinate of the slice area.')
+    parser.add_argument('--slice_height', type=int, help='Height of each slice.')
+    parser.add_argument('--slice_rows', type=int, help='Number of slice rows.')
+    parser.add_argument('--slice_width', type=int, help='Width of each slice.')
+    parser.add_argument('--slice_cols', type=int, help='Number of slice columns.')
+    args = parser.parse_args()
+
+    slice_image(args.image_path, args.output_dir, args.top, args.left, args.bottom, args.right,
+                args.slice_height, args.slice_rows, args.slice_width, args.slice_cols)
